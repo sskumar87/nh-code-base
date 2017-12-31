d08b32e8aa6ccad86f6b0076838124618fbcbed3
Shyam <shyam.sawarna@gmail.com>
2017-12-31 15:57:27 +0530

    init

---
+++ src/main/java/com/nh/NandwaniHandloomApplication.java
@@ -0,0 +1,12 @@
+package com.nh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NandwaniHandloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NandwaniHandloomApplication.class, args);
+	}
+}
